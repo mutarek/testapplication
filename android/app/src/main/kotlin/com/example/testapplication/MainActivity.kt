456f278dda7916fda2583f491a7c4e945e126e2f
mohiu <mohiuddintarek2017@gmail.com>
2021-11-21 21:58:33 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: FirstPage(),
+  ));
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
